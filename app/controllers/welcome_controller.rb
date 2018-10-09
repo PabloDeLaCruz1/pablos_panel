cac4e701304c6ed6be5f7f771b5ccbbeaaa22514
Pablo De La Cruz <pdelac@ramapo.edu>
2018-10-09 14:38:47 -0400

    AWESOME BACKGROUND :OOOO

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
 
-    @array =[1,2,3,5]
-    @id = params[:id]
-    @page = params[:page]
+    # @array =[1,2,3,5]
+    # @id = params[:id]
+    # @page = params[:page]
     
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2018_10_05_165722) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cohorts", force: :cascade do |t|
     t.string "name"
     t.date "start_date"
@@ -43,6 +50,25 @@ ActiveRecord::Schema.define(version: 2018_10_05_165722) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", id: :serial, force: :cascade do |t|
+    t.string "confirmation", limit: 50
+    t.boolean "paid"
+    t.integer "number"
+    t.datetime "date_created", default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "product_orders", id: :serial, force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.datetime "date_created", default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "products", id: :serial, force: :cascade do |t|
+    t.string "sku", limit: 50
+    t.string "name", limit: 50
+    t.datetime "date_created", default: -> { "CURRENT_TIMESTAMP" }
+  end
+
   create_table "studentcohorts", force: :cascade do |t|
     t.bigint "cohort_id"
     t.bigint "student_id"
@@ -69,5 +95,6 @@ ActiveRecord::Schema.define(version: 2018_10_05_165722) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "product_orders", "orders", name: "product_orders_order_id_fkey"
+  add_foreign_key "product_orders", "products", name: "product_orders_product_id_fkey"
 end
-
